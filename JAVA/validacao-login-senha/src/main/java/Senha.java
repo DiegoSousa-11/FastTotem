538a6ff6ddacc60823003af61b3f86f0da5960e7
Isabel Alberti <125396002+IsabelCAlberti@users.noreply.github.com>
2023-09-04 01:00:52 -0300

    isabelCalberti - Finalizando validação de login

--- JAVA/validacao-login-senha/src/main/java/Senha.java
+++ JAVA/validacao-login-senha/src/main/java/Senha.java
@@ -2,50 +2,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Senha {
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
-    boolean tamanho;
-    boolean minusculo;
-    boolean maiusculo;
-    boolean digito;
-
-    public String validarEmail(String email) {
-        String validacaoLoginOk = "";
-        for (int i = 0; i < email.length(); i++) {
-            if (Character.isDigit(email.charAt(i)) && Character.isLetter(email.charAt('@')) && email != null) {
-                digito = true;
-            }
-
-            if (digito) {
-                validacaoLoginOk = "E-mail é válido \n";
-            } else {
-                validacaoLoginOk = "Este e-mail não é válido \n";
-            }
+    public static boolean emailValidator(String email) {
+        if (email == "") {
+           System.out.println(" E-mail precisa ser preenchido");
         }
-            return validacaoLoginOk;
-        }
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
+    }
 
 
-    public String validacaoSenha(String senha) {
-        String validacaoOk = "";
-        if (senha.length() >= 8) {
-            tamanho = true;
+    private static final String PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$";
+    /* ^         # start of the string
+ (?=.*\d)        # a digit must occur at least once
+ (?=.*[a-z])     # a lower case letter must occur at least once
+ (?=.*[A-Z])     # an upper case letter must occur at least once
+ .{6,10}          # 6-10 character password, both inclusive
+ $               # end of the string*/
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
+    public static boolean validacaoSenha(String senha) {
+        if (senha == "") {
+            System.out.println(" Senha precisa ser preenchida");
         }
-        for (int i = 0; i < senha.length(); i++) {
-            if (Character.isDigit(senha.charAt(i))) {
-                digito = true;
-            } else if (Character.isUpperCase(senha.charAt(i))) {
-                maiusculo = true;
-            } else if (Character.isLowerCase(senha.charAt(i))) {
-                minusculo = true;
-            }
-        }
-        if (minusculo && maiusculo && tamanho && digito) {
-            validacaoOk = "A senha é válida";
-        } else {
-            validacaoOk = "A senha não é válida.";
-        }
-        return validacaoOk;
+        Matcher matcher = PASSWORD_PATTERN.matcher(senha);
+        return matcher.matches();
     }
 }
-
-
--- JAVA/validacao-login-senha/src/main/java/Test_LoginSenha.java
+++ JAVA/validacao-login-senha/src/main/java/Test_LoginSenha.java
@@ -4,13 +4,27 @@ public class Test_LoginSenha {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Senha validacao = new Senha();
+        String senha;
+        String login;
 
-        System.out.print("Informe seu Login: ");
-        String login = entrada.nextLine();
-        System.out.format(validacao.validarEmail(login));
+        do {
+            System.out.print("Informe seu Login: ");
+            login = entrada.nextLine();
 
-        System.out.print("Informe sua Senha: ");
-        String senha = entrada.nextLine();
-        System.out.format(validacao.validacaoSenha(senha));
+            System.out.print("Informe sua Senha: ");
+            senha = entrada.nextLine();
+
+            if (Senha.emailValidator(login) && (Senha.validacaoSenha(senha))) {
+                System.out.println("*----------*----------*----------* \n" +
+                      "   Login realizado com Sucesso! \n" +
+                      "*----------*----------*----------*");
+            } else if (Senha.emailValidator(login) != true) {
+                System.out.println(" E-mail inválido \n Tente Novamente");
+            } else if (Senha.validacaoSenha(senha) != true){
+                System.out.println(" Senha inválida \n Tente Novamente");
+            }
+        }
+        while (Senha.emailValidator(login) != true || Senha.validacaoSenha(senha) != true);
     }
 }
+
